688eba2ccf4f64503cc1393a3dda9af9aa77cf4c
srinivaskinik <srinivaskinik@gmail.com>
2025-11-19 05:27:02 +0530

    Refactored the project structure.
    CSVReaderUtil: load the csv file and map it to UserBean
    
    UserBean: For csv mapping and will be used by Dataproviders.
    
    Moved the testData from src/main/resources to src/test/resources

---
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -0,0 +1,31 @@
+package com.api.utils;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.dataproviders.api.bean.UserBean;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class CSVReaderUtil {
+//constructor will be private
+//method will be a static
+	public static void loadCSV(String pathOfCSVFile) {
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
+		InputStreamReader isr = new InputStreamReader(is);
+		CSVReader csvReader = new CSVReader(isr);
+		
+		CsvToBean<UserBean> csvToBean = new CsvToBeanBuilder(csvReader)
+				.withType(UserBean.class)
+				.withIgnoreEmptyLine(true)
+				.build();
+		
+		List<UserBean> userList = csvToBean.parse();
+		
+		System.out.println(userList);
+
+	}
+
+}
