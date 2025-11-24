6a6638e891bae5bb2d054bd78aa7b722c6008ddd
srinivaskinik <srinivaskinik@gmail.com>
2025-11-24 06:26:35 +0530

    Added HikariCP into the framework

---
+++ src/test/java/com/database/DemoRunner.java
@@ -0,0 +1,15 @@
+package com.database;
+
+import java.sql.SQLException;
+
+public class DemoRunner {
+	
+	public static void main(String[] args) throws SQLException {
+		DatabaseManager.createConnection();
+		DatabaseManager.createConnection();
+		DatabaseManager.createConnection();
+		DatabaseManager.createConnection();
+
+	}
+
+}
---
+++ src/test/java/com/database/DemoRunner2.java
@@ -0,0 +1,21 @@
+package com.database;
+
+import java.sql.SQLException;
+
+public class DemoRunner2 {
+	
+	public static void main(String[] args) throws SQLException {
+		DatabaseManager.createConnection();
+		long startTime = System.currentTimeMillis();
+		for(int i=1;i<=1000;i++)
+		{
+			DatabaseManager.createConnection();
+			DatabaseManager.createConnection();
+			DatabaseManager.createConnection();
+		}
+		
+		long endTime=System.currentTimeMillis();
+		System.out.println("Duration "+(endTime-startTime)+"ms");
+	}
+
+}
