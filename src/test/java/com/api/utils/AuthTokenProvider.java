ddb8cd7f484ddde1faa8510af1d7b5ae16059d1b
srinivaskinik <srinivaskinik@gmail.com>
2025-12-17 15:31:11 +0530

    Introduced Token Caching in the project.
    Improved exeution time for the tests.

--- src/test/java/com/api/utils/AuthTokenProvider.java
+++ src/test/java/com/api/utils/AuthTokenProvider.java
@@ -7,19 +7,28 @@ import static com.api.constant.Role.SUP;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.api.constant.Role;
 import com.api.request.model.UserCredentials;
 
 import io.restassured.http.ContentType;
 
 public class AuthTokenProvider {
-
+	private static Map<Role,String> tokenCache=new ConcurrentHashMap<Role,String>();
 	private AuthTokenProvider() {
 
 	}
 
 	public static String getToken(Role role) {
 		// make request to login api and extract the token
+		
+		if(tokenCache.containsKey(role)) {
+			return tokenCache.get(role);
+		}
+		
 		UserCredentials userCredentials = null;
 		if (role == FD) {
 			userCredentials = new UserCredentials("iamfd", "password");
@@ -34,6 +43,8 @@ public class AuthTokenProvider {
 				.body(userCredentials).when().post("login").then().log().ifValidationFails().statusCode(200)
 				.body("message", equalTo("Success")).extract().body().jsonPath().getString("data.token");
 
+		tokenCache.put(role, token);
+		
 		return token;
 
 	}
