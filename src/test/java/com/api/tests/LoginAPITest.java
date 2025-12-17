bcc5b038ee4cdcff7ea61157e7ffdb2ae8336fe7
srinivaskinik <srinivaskinik@gmail.com>
2025-12-17 16:07:13 +0530

    Created Auth Service class to maintain login api at one place.

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -1,6 +1,7 @@
 package com.api.tests;
 
-import static io.restassured.RestAssured.given;
+import static com.api.utils.SpecUtil.responseSpec_OK;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 
 import java.io.IOException;
@@ -9,24 +10,24 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.api.request.model.UserCredentials;
-import static com.api.utils.SpecUtil.*;
+import com.api.services.AuthService;
 
-import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import io.restassured.response.Response;
 
 public class LoginAPITest {
 	private UserCredentials userCredentials;
+	private AuthService authService;
 	@BeforeMethod(description = "Create the payload for login API")
 	public void setup() {
 	 userCredentials = new UserCredentials("iamfd", "password");
+	 authService=new AuthService();
 	}
 	@Test(description = "Verifying if the login api is working for user FD user",groups = {"api","regression","smoke"})
 	public void loginAPITest() throws IOException {
 		
-		given()
-			.spec(requestSpec(userCredentials))
-		.when()
-			.post("login")
-		.then()
+		
+		authService.login(userCredentials)
+			.then()
 			.spec(responseSpec_OK())
 		.and()
 			.body("message",equalTo("Success"))
