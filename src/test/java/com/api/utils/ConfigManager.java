2dc4ea0d11fc4a1c16e22041a82ab4858cc8b8e0
srinivaskinik <srinivaskinik@gmail.com>
2026-01-07 18:24:20 +0530

    Added Allure Report to the framework.
    Created a AllureEnvWriterUtil that shows env info in the report.
    Changed the ConfigManager env variable to public

--- src/test/java/com/api/utils/ConfigManager.java
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class ConfigManager {
 	private static Properties prop = new Properties();
 	private static String path = "config/config.properties";
-	private static String env;
+	public static String env;
 	private static final Logger LOGGER = LogManager.getLogger(ConfigManager.class);
 
 	private ConfigManager() {
